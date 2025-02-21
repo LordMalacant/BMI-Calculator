d82ce923a24a58372872ab148f8f7b92decd0fa1
LordMalacant <JuanLuisEspadaReyes@hotmail.com>
2025-02-21 12:08:01 +0100

    Update MainActivity.kt

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -6,15 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var height: EditText
+    lateinit var weight: EditText
+    lateinit var button: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        heightEditText = findViewById(R.id.heightEditText)
+        weightEditText = findViewById(R.id.weightEditText)
+        CalculateButton = findViewById(R.id.CalculatebButton)
+
+        calculateButton.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("height", height.text.toString())
+            intent.putExtra("weight", weight.text.toString())
+
         }
     }
 }
